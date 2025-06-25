3ba74152b06a69f2e2490360890078ab2d860198
Damin <min_0625@kakao.com>
2025-06-25 10:15:20 +0900

    [⚙️ FEATURE] : main 화면 입력 기능 생성

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,18 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("언제나 점심을 책임지는 \uD83C\uDF5D LAUNCHMATE 입니다.");
+        System.out.println("원하시는 음식점을 선택해주세요.\n");
+        System.out.println("1. 엄마의 밥상");
+        System.out.println("2. 부리또리");
+        System.out.println("3. 고향촌 칼국수\n");
+
+        System.out.print("입력 : ");
+        int num = sc.nextInt();
+        System.out.println(num);
+    }
 }
