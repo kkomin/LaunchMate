fcf251f44eff32f51cb43a5f56e3238aea2ad3a4
Damin <min_0625@kakao.com>
2025-06-25 14:38:12 +0900

    [⚠ FIXED] : 커밋 오류 수정

--- src/model/OrderItem.java
+++ src/model/OrderItem.java
@@ -4,7 +4,7 @@ package model;
 // 주문 시 결재까지의 계산도 포함
 public class OrderItem {
     // id, name, price -> Menu에 이미 존재함
-    private Menu menu;
+    private final Menu menu;
     private int count;
 
     public OrderItem(Menu menu, int count) {
