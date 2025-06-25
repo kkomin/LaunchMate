0f4f3adaff50067c99671cfd5262c416bfe70478
Damin <min_0625@kakao.com>
2025-06-25 13:48:13 +0900

    [⚙️ FEATURE] : 선택한 메뉴 저장

--- src/model/OrderItem.java
+++ src/model/OrderItem.java
@@ -1,6 +1,30 @@
 package model;
 
 // 주문하기 위해 선택한 아이템들
+// 주문 시 결재까지의 계산도 포함
 public class OrderItem {
+    // id, name, price -> Menu에 이미 존재함
+    private Menu menu;
+    private int count;
 
+    public OrderItem(Menu menu, int count) {
+        this.menu = menu;
+        this.count = count;
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int totalPrice() {
+        return menu.getPrice() * count;
+    }
 }
