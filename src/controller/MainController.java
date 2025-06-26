4ce161092af0b3c3fb88076bfaa392347f691201
Damin <min_0625@kakao.com>
2025-06-26 13:51:08 +0900

    [⚙️ FEATURE] : 메인 홈 구성 변경

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,7 +1,44 @@
 package controller;
 
+import login.SignUp;
+
+import java.util.Scanner;
+
 public class MainController {
     public static void start() {
+        Scanner sc = new Scanner(System.in);
 
+        while(true) {
+            System.out.println("언제나 점심을 책임지는 \uD83C\uDF5D LAUNCHMATE 입니다.");
+            System.out.println("1. 로그인");
+            System.out.println("2. 회원가입");
+            System.out.println("3. 탈퇴하기");
+            System.out.println("4. 프로그램 종료");
+
+            System.out.print("입력 : ");
+            int input = 0;
+
+            try {
+                input = sc.nextInt();
+            } catch (Exception e) {
+                System.out.println("숫자만 입력하세요.\n");
+            }
+
+            switch(input) {
+                case 1 :
+                    // 로그인
+                    break;
+                case 2 :
+                    // 회원가입
+                    break;
+                case 3 :
+                    // 탈퇴하기
+                    break;
+                default:
+                    System.out.println("다시 입력하세요.");
+                    continue;
+            }
+            break;
+        }
     }
 }
