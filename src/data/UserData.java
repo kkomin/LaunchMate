7462146f40cab1591ba990cdf1cb023192c8146b
Damin <min_0625@kakao.com>
2025-06-26 14:08:40 +0900

    [⚙️ FEATURE] : 사용자 목록 저장 기능 구현

--- src/data/UserData.java
+++ src/data/UserData.java
@@ -1,4 +1,11 @@
 package data;
 
+import model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 사용자 목록 저장
 public class UserData {
+    public static List<User> userList = new ArrayList<>();
 }
