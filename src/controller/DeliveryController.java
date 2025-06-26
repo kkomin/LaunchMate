5073b7045d464b782a64f6ca8a65d7d025775a9f
Damin <min_0625@kakao.com>
2025-06-26 10:38:50 +0900

    [️🔧 LITTLE_FIXED] : 예상 시간에 따른 메세지 출력 위치 변경 & 예외문 추가

--- src/controller/DeliveryController.java
+++ src/controller/DeliveryController.java
@@ -10,6 +10,7 @@ public class DeliveryController {
         }
         System.out.println("👍 배달 기사님 배정 완료!\n");
 
+        System.out.println("======================================");
         System.out.println("📦 배달을 시작합니다.");
 
         // 10초 ~ 2분 사이로 걸리는 시간 (deliveryTime), 거리(distance)
@@ -18,6 +19,22 @@ public class DeliveryController {
 
         // 예상 시간
         System.out.printf("예상 시간 : %d 초\n",deliveryTime / 1000);
+        System.out.println("======================================");
+
+        // 배달 시간이 30초 이상일 경우
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            System.out.println("😅 배달 기사님이 배정을 취소하셨습니다.");
+        }
+
+        if(deliveryTime >= 30000) {
+            System.out.println("\n\uD83C\uDF27\uFE0F 현재 폭우로 인해 기사님이 안전운전 중이에요!\n");
+        }
+        // 배달 시간이 30초 미만일 경우
+        else {
+            System.out.println("\n🛵 기사님이 열심히 달려가고 있어요!\n");
+        }
 
         // 단계 수 설정 ( 배달 시간이 길면 5단계, 짧으면 10단계)
         int step = (deliveryTime < 30000) ? 5 : 10;
@@ -44,15 +61,6 @@ public class DeliveryController {
         // 배달 완료 메세지
         System.out.println("\n\uD83C\uDF5D LAUNCHMATE");
         System.out.println("🏠 배달이 완료되었습니다. 맛있게 드세요!");
-
-        // 배달 시간이 30초 이상일 경우
-        if(deliveryTime >= 30000) {
-            System.out.println("\uD83C\uDF27\uFE0F 현재 폭우로 인해 기사님이 안전운전 중이에요!");
-        }
-        // 배달 시간이 30초 미만일 경우
-        else {
-            System.out.println("🛵 기사님이 열심히 달려가고 있어요!");
-        }
     }
     // 배달 시간 랜덤 설정
     private static int getRandomDelivery() {
