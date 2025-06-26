e300d0fc79fe4459f71fa6c159eb31f85364aac1
Damin <min_0625@kakao.com>
2025-06-26 14:18:24 +0900

    [⚙️ FEATURE] : name, id 중복 검사 수행

--- src/login/SignUp.java
+++ src/login/SignUp.java
@@ -1,6 +1,8 @@
 package login;
 
 import controller.MainController;
+import data.UserData;
+import model.User;
 
 import java.util.Scanner;
 
@@ -15,13 +17,25 @@ public class SignUp {
             System.out.print("아이디 : ");
             String id = sc.nextLine();
 
+            // 중복 검사 -> 이름 및 아이디 중복일 경우
+            boolean isDuplicate = false;
+            for(User user : UserData.userList) {
+                if(user.getName().equals(name) && user.getId().equals(id)) {
+                    System.out.println("🙅‍♀️ 이미 존재하는 사용자 입니다.");
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if(isDuplicate) {
+                continue;
+            }
+
             System.out.print("비밀번호 : ");
             String pw = sc.nextLine();
 
+            System.out.println("✅ 회원가입이 완료되었습니다!");
             break;
         }
-
-        System.out.println("✅ 회원가입이 완료되었습니다!");
         MainController.start();
     }
 }
